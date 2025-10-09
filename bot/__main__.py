d37447a3d1cb774a89298197d1a0e6ddc2fa7439
KseniyaBeskhmelnova <kuks_2004@mail.ru>
2025-10-09 12:02:08 +0300

    Telegram echo bot

---
+++ bot/__main__.py
@@ -0,0 +1,36 @@
+import bot.database_client
+import bot.telegram_client
+import time
+
+def main() -> None:
+    next_update_offset = 0
+    try:
+        while True:
+            updates = bot.telegram_client.getUpdates(next_update_offset)
+            bot.database_client.persist_updates(updates)
+            for update in updates:
+                message = update.get("message", {})
+                chat_id = message.get("chat", {}).get("id")
+                if not chat_id:
+                    continue
+                if "text" in message:
+                    bot.telegram_client.sendMessage(
+                        chat_id=update["message"]["chat"]["id"],
+                        text=update["message"]["text"],
+                    )
+                    print("T", end="", flush=True)
+                elif "sticker" in message:
+                    sticker_file_id = message["sticker"]["file_id"]
+                    bot.telegram_client.sendSticker(
+                        chat_id=chat_id,
+                        sticker_file_id=sticker_file_id
+                    )
+                    print("S", end="", flush=True)
+
+                next_update_offset = max(next_update_offset, update["update_id"] + 1)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\nBye!")
+
+if __name__ == "__main__":
+    main()
